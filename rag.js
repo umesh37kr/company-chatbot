f60b218485278712b2a69c94e96eca9dc39160f2
umesh <umesh37kr@gmail.com>
2025-09-07 17:02:33 +0530

    inital commit

---
+++ rag.js
@@ -0,0 +1,14 @@
+/**
+ * Implementation plan
+ * Stage 1: Indexing
+ * 1. Load the document - pdf, text
+ * 2. Chunk the document
+ * 3. Generate Vector Embeddings
+ * 4. Store the vector embeddings - vector database
+ *
+ * Stage 2 Using the chatbot
+ * 1. setup LLM
+ * 2. Add retrieval step
+ * 3. Pass input + retrieval information to LLM
+ * 4. Congratulations
+ */
