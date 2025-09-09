598f7e52ed934f66c3a269e0f8e1dc5dfe3fec19
umesh <umesh37kr@gmail.com>
2025-09-09 09:56:43 +0530

    chunks the documents

--- prepare.js
+++ prepare.js
@@ -1,8 +1,13 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 export async function indexTheDocument(filePath) {
   const loader = new PDFLoader(filePath, { splitPages: false });
   const docs = await loader.load();
   console.log(docs);
-  //   console.log(docs[0].metadata);
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+  const texts = await textSplitter.splitText(docs[0].pageContent);
+  //   console.log(texts);
 }
