88210400bf9958009cc9e504eb901091c9c6dd9d
umesh <umesh37kr@gmail.com>
2025-09-14 12:42:51 +0530

    added pinecone vector database

--- prepare.js
+++ prepare.js
@@ -1,13 +1,35 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+
+const embeddings = new OpenAIEmbeddings({
+  model: "text-embedding-3-small",
+});
+
+const pinecone = new PineconeClient();
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  pineconeIndex,
+  maxConcurrency: 5,
+});
+
 export async function indexTheDocument(filePath) {
   const loader = new PDFLoader(filePath, { splitPages: false });
   const docs = await loader.load();
-  console.log(docs);
+  // console.log(docs[0].pageContent);
   const textSplitter = new RecursiveCharacterTextSplitter({
     chunkSize: 500,
     chunkOverlap: 100,
   });
   const texts = await textSplitter.splitText(docs[0].pageContent);
-  //   console.log(texts);
+
+  const documents = texts.map((chunk) => {
+    return {
+      pageContent: chunk,
+      metadata: docs[0].metadata,
+    };
+  });
+  await vectorStore.addDocuments(documents);
 }
